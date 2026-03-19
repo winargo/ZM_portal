d740761ead635aa76e7cabfd0971a63812061df8
winargo <rwinargo97@gmail.com>
2026-03-20 01:16:21 +0800

    Update googleplus.php

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -3,8 +3,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['googleplus']['application_name'] = 'Kaspro';
-$config['googleplus']['client_id']        = '529692671282-sce8v58s83pvou16vtnub5macs0naqcp.apps.googleusercontent.com';
-$config['googleplus']['client_secret']    = 'QGvstyZ1M1z4MBvwKbVLEpO3';
+$config['googleplus']['client_id']        = '';
+$config['googleplus']['client_secret']    = '';
 $config['googleplus']['redirect_uri']     = 'http://localhost/user_authentication';
-$config['googleplus']['api_key']          = 'AIzaSyDgpdpozJbt_dcuQ0ep2mH6I0wA19XsjfU';
+$config['googleplus']['api_key']          = '';
 $config['googleplus']['scopes']           = array();
